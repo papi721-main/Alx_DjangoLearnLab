ad75c702f85ee069a5f4bc283f84d5d26a8a03a8
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-07-20 20:38:32 +0300

    feat: define set of models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Author: {self.name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author.name}"
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return f"Library: {self.name}"
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return f"Librarian: {self.name} at {self.library.name}"
