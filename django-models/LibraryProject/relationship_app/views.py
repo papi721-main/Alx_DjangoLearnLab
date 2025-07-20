c518bbc998a21a6b16cf0efe5ac0ea8f4ba4be86
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-07-20 23:14:30 +0300

    feat: write functiona and class based views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book, Library
 
-# Create your views here.
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view to list all libraries
+class LibraryListView(ListView):
+    model = Library
+    template_name = 'relationship_app/library_list.html'
+    context_object_name = 'libraries'
